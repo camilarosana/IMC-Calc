0a9680720bc22c1197da7550eb062245c448b1dc
Camila Rosana <camilarosana95@gmail.com>
2023-09-10 21:08:59 -0300

    modificação na linha 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,44 @@
- 
-
-
-
-
-
-
-
-
-
-
+const data = [
+    {
+      min: 0,
+      max: 18.4,
+      description: "Menor que 18,5",
+      info: "Magreza",
+    },
+    {
+      min: 18.5,
+      max: 24.9,
+      description: "Entre 18,5 e 24,9",
+      info: "Normal",
+    },
+    {
+      min: 25,
+      max: 29.9,
+      description: "Entre 25,0 e 29,9",
+      info: "Sobrepeso",
+    },
+    {
+      min: 30,
+      max: 39.9,
+      description: "Entre 30,0 e 39,9",
+      info: "Obesidade",
+    },
+    {
+      min: 40,
+      max: 99,
+      description: "Maior que 40,0",
+      info: "Obesidade grave",
+    },
+  ]; 
 
  const form = document.querySelector('#form');
  const weight = document.querySelector('#weight');
  const height = document.querySelector('#height');
  const calculateBtn = document.querySelector('#calculate');
  const clearBtn = document.querySelector('#clear');
+
+ // funções
+
+ // inicialização
+
+ // eventos
