6aeaaf633cffe726be9abf7a554cd14436252883
Camila Rosana <camilarosana95@gmail.com>
2023-09-10 21:02:33 -0300

    Criação dos const no js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
  
+
+
+
+
+
+
+
+
+
+
+
+ const form = document.querySelector('#form');
+ const weight = document.querySelector('#weight');
+ const height = document.querySelector('#height');
+ const calculateBtn = document.querySelector('#calculate');
+ const clearBtn = document.querySelector('#clear');
